a7e6f972dda4e5d4eeb1d3fead13d0a75f3eee93
QingXuanLIU <313799288@qq.com>
2013-06-27 11:31:22 +0800

    第二次上传
    setbackgroundcolor

--- UITableView_page/ViewController.m
+++ UITableView_page/ViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor scrollViewTexturedBackgroundColor]];
     self.myTableView.dataSource=self;
     self.myTableView.delegate=self;
     items=[[NSMutableArray alloc] initWithCapacity:0];
