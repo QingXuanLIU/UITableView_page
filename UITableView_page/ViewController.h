c83065b36620bcedad816ce76b0a7a3b9f206dea
QingXuanLIU <313799288@qq.com>
2013-06-27 11:23:32 +0800

    第一次上传
    UITableView分页显示

--- UITableView_page/ViewController.h
+++ UITableView_page/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+{
+    IBOutlet UITableView *myTableView;
+    NSMutableArray *items;
+}
+@property(retain, nonatomic)IBOutlet UITableView *myTableView;
 
 @end
--- UITableView_page/ViewController.m
+++ UITableView_page/ViewController.m
@@ -13,17 +13,83 @@
 @end
 
 @implementation ViewController
+@synthesize myTableView;
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.myTableView.dataSource=self;
+    self.myTableView.delegate=self;
+    items=[[NSMutableArray alloc] initWithCapacity:0];
+    for (int i=0; i<10; i++) {
+        [items addObject:[NSString stringWithFormat:@"cell %i",i]];
+//        NSLog(@"%@",items);
+    }
 }
 
-- (void)didReceiveMemoryWarning
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    int count = [items count];
+//    NSLog(@"%d",[items count]);
+    return  count + 1;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *tag=@"tag";
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:tag];
+    if (cell==nil) {
+        cell=[[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tag] autorelease];
+    }
+    if([indexPath row] == ([items count])) {
+        //创建loadMoreCell
+        cell.textLabel.text=@"More..";
+    }else {
+        cell.textLabel.text=[items objectAtIndex:[indexPath row]];
+    }
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == [items count]) {
+        UITableViewCell *loadMoreCell=[tableView cellForRowAtIndexPath:indexPath];
+        loadMoreCell.textLabel.text=@"loading more …";
+        [self performSelectorInBackground:@selector(loadMore) withObject:nil];
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        return;
+    }
+    //其他cell的事件
+    
+}
+-(void)loadMore
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSMutableArray *more;
+    more=[[NSMutableArray alloc] initWithCapacity:0];
+    for (int i=0; i<10; i++) {
+        [more addObject:[NSString stringWithFormat:@"cell ++%i",i]];
+    }
+    //加载你的数据
+    [self performSelectorOnMainThread:@selector(appendTableWith:) withObject:more waitUntilDone:NO];
+    [more release];
 }
-
+-(void) appendTableWith:(NSMutableArray *)data
+{
+    for (int i=0;i<[data count];i++) {
+        [items addObject:[data objectAtIndex:i]];
+    }
+    NSMutableArray *insertIndexPaths = [NSMutableArray arrayWithCapacity:10];
+    for (int ind = 0; ind < [data count]; ind++) {
+        NSIndexPath *newPath =  [NSIndexPath indexPathForRow:[items indexOfObject:[data objectAtIndex:ind]] inSection:0];
+        [insertIndexPaths addObject:newPath];
+    }
+    [self.myTableView insertRowsAtIndexPaths:insertIndexPaths withRowAnimation:UITableViewRowAnimationFade];
+    
+}
+- (void)viewDidUnload {
+    items=nil;
+    self.myTableView=nil;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+- (void)dealloc {
+    [self.myTableView release];
+    [items release];
+    [super dealloc];
+}
 @end
