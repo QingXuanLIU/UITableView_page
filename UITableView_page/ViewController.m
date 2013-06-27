dff12dce40e632b81424e6126763176f0d3f7cd8
QingXuanLIU <313799288@qq.com>
2013-06-27 11:36:04 +0800

    Revert "Revert "Revert "Revert "Revert "第二次上传"""""
    This reverts commit c418ac4901fa77c453c8925726d22181c4482849.

--- UITableView_page/ViewController.m
+++ UITableView_page/ViewController.m
@@ -17,7 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view setBackgroundColor:[UIColor scrollViewTexturedBackgroundColor]];
     self.myTableView.dataSource=self;
     self.myTableView.delegate=self;
     items=[[NSMutableArray alloc] initWithCapacity:0];
